21623cb1dc8fd2bb90b16931ada81763021bf98b
DD <dpd_damyanov@abv.bg>
2015-10-20 17:05:31 +0300

    test

---
+++ gh_releaser.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+#Create the release
+curl -H "Authorization: token $GITHUB_OAUTH_TOKEN" -H "Content-Type: application/json" -d '{ "tag_name": "v0.0.$CIRCLE_BUILD_NUM", "target_commitish": "develop", "name": "v0.0.$CIRCLE_BUILD_NUM", "body": "Release descrinption" }'  -X POST https://api.github.com/repos/TumbaSolutions/ref-ios/releases
+
+#Strip the upload URL from the release data
+UPLOAD_URL=`curl -H "Authorization: token $GITHUB_OAUTH_TOKEN" https://api.github.com/repos/TumbaSolutions/ref-ios/releases/tags/v0.0.$CIRCLE_BUILD_NUM | grep upload_url | awk '{print $2}' | sed -r 's/\{\?name,label\}",?|"//g'`
+
+#Upload the apk to Github releases
+curl -H "Authorization: token $GITHUB_OAUTH_TOKEN" -H "Content-Type: application/vnd.android.package-archive" -d @app/build/outputs/app-release.apk -X POST $UPLOAD_URL?name=Ref-Android-App
+
