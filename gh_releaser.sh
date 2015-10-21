8899d14439abd1a72a23739914f14d8083e09300
DD <dpd_damyanov@abv.bg>
2015-10-21 12:08:26 +0300

    test

--- gh_releaser.sh
+++ gh_releaser.sh
@@ -3,16 +3,16 @@
 REQUEST_BODY='{ "tag_name": "v0.0.'"$CIRCLE_BUILD_NUM"'", "target_commitish": "master", "name": "v0.0.'"$CIRCLE_BUILD_NUM"'", "body": "Release descrinption" }'
 HEADER1="Content-Type: application/json"
 HEADER2="Authorization: token $GITHUB_OAUTH_TOKEN"
-HEADER3="Content-Type: application/vnd.android.package-archive"
-ARTIFACT="@app/build/outputs/app-release.apk"
+HEADER3="Content-Type: application/octet-stream"
+ARTIFACT="@app/build/outputs/apk/app-release.apk"
 REQUEST_URL="https://api.github.com/repos/TumbaSolutions/ref-android/releases"
 
 #Create the release
-curl -H $HEADER2 -H $HEADER1 -d $REQUEST_BODY  -X $POST $REQUEST_URL
+#curl -H "$HEADER2" -H "$HEADER1" -d "$REQUEST_BODY"  -X POST "$REQUEST_URL"
 
 #Strip the upload URL from the release data
 UPLOAD_URL=`curl -H "Authorization: token $GITHUB_OAUTH_TOKEN" $REQUEST_URL/tags/v0.0.$CIRCLE_BUILD_NUM | grep upload_url | awk '{print $2}' | sed -r 's/\{\?name,label\}",?|"//g'`
 
 #Upload the apk to Github releases
-curl -H $HEADER2 -H $HEADER3 -d $ARTIFACT -X POST $UPLOAD_URL?name=Ref-Android-App
+curl -H "$HEADER2" -H "$HEADER3" -d $ARTIFACT -X POST $UPLOAD_URL?name=Ref-Android-App
 
